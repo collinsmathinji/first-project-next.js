c30605f3502f02785c97ca83c8cb3e9b977c3ba2
collinsmathinji <125723113+collinsmathinji@users.noreply.github.com>
2024-01-21 15:30:47 +0300

    full commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      NEXTAUTH_SECRET: '4jOOkxmnCxQzeOsbeT9KbyVCIOGYsVMc3op5gANlzVYzd8UgXn1UO/G2w+nC+W8m',
+    },
+  };
+  
+  export default nextConfig;
+  
---
+++ src/app/auth/profile/page.tsx
@@ -0,0 +1,28 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+const ProfilePage = async () => {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  return (
+    <div>
+      <Image
+        height={300}
+        width={300}
+        src={user?.image ?? ""}
+        alt={user?.firstname ?? ""}
+        className="rounded-full"
+      />
+      <div className="grid grid-cols-4 gap-y-4">
+        <p>First Name:</p> <p className="col-span-3">{user?.firstname}</p>
+        <p>Last Name:</p> <p className="col-span-3">{user?.lastname}</p>
+        <p>Phone:</p> <p className="col-span-3">{user?.phonenumber}</p>
+        <p>Email:</p> <p className="col-span-3">{user?.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
